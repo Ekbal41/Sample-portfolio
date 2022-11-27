5da2794009e31fbacf2202b35ea74f19e153d1cf
Ekbal41 <mdasifekbalshagor@gmail.com>
2022-11-27 23:13:18 +0600

    bug update

--- simpleportfolio/settings.py
+++ simpleportfolio/settings.py
@@ -55,7 +55,8 @@ ROOT_URLCONF = 'simpleportfolio.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR,'templates'],
+        #'DIRS': [BASE_DIR,'templates'],
+        'DIRS': [BASE_DIR,'Sample-portfolio/templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
